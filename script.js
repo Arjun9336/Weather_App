781998eb2f827ff95d4da4336001f62d081934cf
Arjun Singh Gaur <arj9336@gmail.com>
2025-01-16 13:36:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ async function checkWeather(city) {
         else if (data.weather[0].main == "Mist") {
             weatherIcon.src = "mist.png";
         }
+        else if(data.weather[0].main == "Snow"){
+            weatherIcon.src = "snow.png";
+        }
 
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
